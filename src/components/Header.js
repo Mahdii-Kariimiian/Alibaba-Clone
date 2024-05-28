5b8a3b6584572d472028dc46c975a99d54e6fc1c
Mahdi Karimian <mahdii.kariimiian@gmail.com>
2024-05-28 14:18:56 +0200

    add Hero section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,37 @@
-import React from 'react'
+import React from "react";
 import { GrLanguage } from "react-icons/gr";
 import { CgProfile } from "react-icons/cg";
-import { countries } from 'country-flag-icons'
+import ItalyFlag from "../assets/Flag_of_Italy.png";
+import LogoIcon from "../assets/Logo.png";
 
 const Header = () => {
-  return (
-    <header className='px-10 py-2 flex text-white bg-black items-center gap-6'>
-      <div >
-        <img
-          src="../assets/Logo.png"
-          className=""
-          alt="Alibaba logo"
-        />
-        
-      </div>
-      <div className='ml-auto'>
-        <p>Deliver to:</p>
-        <p>
-          <img src="../assets/Flag_of_Italy.png" alt='Flag of Italy '></img> IT</p>
-      </div>
+    return (
+        <header className="absolute top-0 right-0 left-0 px-10 py-4 flex text-white items-center gap-6 bg-gradient-to-b from-black to-[#ffffff00]">
+            <div className="w-[210px]">
+                <img src={LogoIcon} className="" alt="Alibaba logo" />
+            </div>
+            <div className="ml-auto">
+                <p className="text-xs">Deliver to:</p>
+                <div className="w-[20px] flex items-center gap-1">
+                    <img src={ItalyFlag} alt="Flag of Italy "></img> <p> IT </p>
+                </div>
+            </div>
 
-      <div className='flex items-center gap-2'>
-      <GrLanguage />
-        English-EUR
-      </div>
+            <div className="flex items-center gap-2">
+                <GrLanguage />
+                English-EUR
+            </div>
 
-      <div  className='flex items-center gap-2'>
-      <CgProfile />
-        Sign in 
-      </div>
+            <div className="flex items-center gap-2">
+                <CgProfile />
+                Sign in
+            </div>
 
-      <button className='px-8 py-1 text-white bg-orange-500 rounded-full'>
-        Sign up
-      </button>
-    </header>
-  )
-}
+            <button className="px-10 py-1 text-white bg-orange-500 rounded-full">
+                Sign up
+            </button>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import { useState } from "react";
+import { FaCirclePlay } from "react-icons/fa6";
+import { FaCamera } from "react-icons/fa";
+
+
 
 const Hero = () => {
-  return (
-    <div>
-      Hero
-    </div>
-  )
-}
+    const [frequentlySearched, setFrequentlySearched] = useState([
+        "electric scooter",
+        "iphone 15 pro max",
+        "alibaba in italia",
+    ]);
 
-export default Hero
+    return (
+        <div className={`bg-sky-800 text-white h-[89vh] pt-20 px-10 space-y-8 flex flex-col justify-center `} >
+            <div className="flex gap-2 items-center text-xl">
+                <FaCirclePlay />
+                <p>Learn about Alibaba.com</p>
+            </div>
+            <h1 className="max-w-[1000px] leading-[60px] text-5xl font-bold">
+                The leading B2B ecommerce platform for global trade
+            </h1>
+            <div className="bg-white flex items-center gap-3 p-1 rounded-full max-w-[800px]">
+                <input
+                    type="text"
+                    className="flex-1 ml-3 p-2 "
+                    placeholder="Search"
+                ></input>
+                <FaCamera className="text-gray-500" />
+
+                <button className="px-8 py-2 text-white bg-orange-500 rounded-full">
+                    Search
+                </button>
+            </div>
+            <div className="flex gap-8 items-center">
+                <h3>Frequently searched:</h3>
+                <div className="flex gap-4">
+                    {frequentlySearched.map((search) => {
+                        return (
+                            <p className="py-2 px-4 border border-white rounded-full text-sm">
+                                {search}
+                            </p>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
